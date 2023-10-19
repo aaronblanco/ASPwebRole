035956fb3aa80b71d21de244fad2b5622982789e
aaronblanco <aaronba13@hotmail.com>
2023-10-19 12:32:12 +0200

    Agregar archivos de proyecto.

---
+++ ASPwebRole/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPwebRole.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
